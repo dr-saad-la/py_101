ce314c9cb9d51bbee18abcd0ef80ff5abc8944e4
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-10-16 18:05:06 +0100

    update

---
+++ mod_01_intro/help_method_practice.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# =======================================================================
+# Author: Dr. Saad Laouadi
+# Course: Python-101
+# Lesson: help_method
+#
+# Description:
+# This module aims to show how to use `help()` method in Python
+#
+# © Copyright: Dr. Saad Laouadi
+# All Rights Reserved 🛡️
+# =======================================================================
+
+# Check the help documentation
+help()
+
+# run the help on ipython or python console
+
+# Check the help of print
+help(print)
---
+++ mod_01_intro/recap.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+help(print)
+
+print(42, "A", "Python Course")
+
+# Writing Comments in Python
+
+print("Something") # This is a simple print statement
+
+# Multi-line comments
+# line one
+# line two
+# ....
+
+"""
+This is a documentation string
+"""
